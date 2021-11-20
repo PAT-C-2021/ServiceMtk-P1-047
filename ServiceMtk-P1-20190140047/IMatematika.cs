7e61c8578df9c0b7391b57f8713ecc82b05a1c4d
fuadrizkani <fuadrizkani22@gmail.com>
2021-11-20 16:15:41 +0700

    Add project files.

---
+++ ServiceMtk-P1-20190140047/IMatematika.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ServiceMtk_P1_20190140047
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IMatematika
+    {
+        //menambahkan faultContract
+        [OperationContract]
+        [FaultContract(typeof(MathFault))] //membuat kelas yang melambangkan exception
+        /*output*/
+        int Tambah(int a, int b); //method
+
+        [OperationContract]
+        [FaultContract(typeof(MathFault))] //membuat kelas yang melambangkan exception
+        /*output*/
+        int Kurang(int a, int b); //method
+
+        [OperationContract]
+        [FaultContract(typeof(MathFault))] //membuat kelas yang melambangkan exception
+        /*output*/
+        int Kali(int a, int b); //method
+
+        [OperationContract]
+        [FaultContract(typeof(MathFault))] //membuat kelas yang melambangkan exception
+        /*output*/
+        int Bagi(int a, int b); //method
+
+        [OperationContract]
+        [FaultContract(typeof(MathFault))] //membuat kelas yang melambangkan exception
+        /*output*/
+        Koordinat TKoordinat(Koordinat a, Koordinat b); //method
+
+    }
+    [DataContract]
+    //menambahkan class
+    class MathFault
+    {
+        [DataMember]
+        public string Kode { get; set; }
+        [DataMember]
+        public string Pesan { get; set; }
+    }
+
+    public class Koordinat
+    {
+        private int _x, _y; //atribut
+        [DataMember]//membuka koordinat, x,y
+        public int X //properties
+        {
+            get { return _x; }
+            set { _x = value; }
+        }
+        [DataMember]
+        public int Y //properties
+        {
+            get { return _y; }
+            set { _y = value; }
+        }
+    }
+
+}
